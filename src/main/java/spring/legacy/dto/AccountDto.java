9505bb37982fbfc226faa16bf8d0f2781d1173e5
ssac17 <84308365+ssac17@users.noreply.github.com>
2025-12-25 09:26:47 +0900

    add spring security

--- src/main/java/spring/legacy/dto/AccountDto.java
+++ src/main/java/spring/legacy/dto/AccountDto.java
@@ -1,8 +1,18 @@
 package spring.legacy.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class AccountDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+
+
     private String password;
 
     public String getName() {
